aa82cc7c36acb641b8e37b0bfae6decb6026cbee
isaias-pv <alemais2003312@gmail.com>
2023-11-18 16:41:39 -0500

    Converción de datos en todos

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -1,7 +1,7 @@
 import { validate } from "../schemas/transaction.js";
 import { TransactionModel } from "./../models/mysql/transaction.js";
-import { request } from 'express';
-import { randomUUID } from 'crypto';
+import { request } from "express";
+import { randomUUID } from "crypto";
 import { convertDateToSaveDB } from "../utils/date.js";
 
 export class TransactionController {
@@ -16,16 +16,16 @@ export class TransactionController {
 
 	orderTransactionsByDate(transactions = []) {
 		const response = {};
-	
-		transactions.forEach(transaction => {
+
+		transactions.forEach((transaction) => {
 			const key = transaction.date_transaction;
-	
+
 			if (!response[key]) {
 				response[key] = { group_key: key, content: [] };
 			}
-			
+
 			response[key].content.push(transaction);
-		});    
+		});
 		return Object.values(response);
 	}
 
@@ -34,23 +34,56 @@ export class TransactionController {
 
 		const response = {};
 
-		transactions.forEach(transaction => {
+		transactions.forEach((transaction) => {
 			const key = transaction.type_transaction_name;
-	
+
 			if (!response[key]) {
 				response[key] = { group_key: key, content: [] };
 			}
-			
+
 			response[key].content.push(transaction);
 		});
-		
-		
-		// const response = {
-		// 	incomes: this.orderTransactionsByDate(transactions.filter(transaction => transaction.concept_id === 1)),
-		// 	expenses: this.orderTransactionsByDate(transactions.filter(transaction => transaction.concept_id === 2))
-		// };
 
-		res.json(Object.values(response));
+		const transactionsValues = Object.values(response);
+
+		const allTransactions =
+			transactionsValues.find((r) => r.group_key === "ALL").content[0]
+				.transactions || [];
+
+		const transactionsMap = new Map();
+
+		allTransactions.map((transaction) => {
+			if (transaction.code_transaction) {
+				if (transactionsMap.has(transaction.code_transaction)) {
+					const existingTransaction = transactionsMap.get(
+						transaction.code_transaction
+					);
+					existingTransaction.type_transaction_name = "TRANSFER";
+				} else {
+					transactionsMap.set(
+						transaction.code_transaction,
+						transaction
+					);
+				}
+			}
+		});
+
+		transactionsValues.find(
+			(r) => r.group_key === "ALL"
+		).content[0].transactions = Array.from(transactionsMap.values()).sort(
+			(a, b) => {
+				if (
+					new Date(a.date_transaction).getTime() >
+					new Date(b.date_transaction).getTime()
+				) {
+					return -1;
+				}
+
+				return 1;
+			}
+		);
+
+		res.json(transactionsValues);
 	};
 
 	getRecents = async (req, res) => {
@@ -64,7 +97,9 @@ export class TransactionController {
 	};
 
 	getTransationByCode = async (req = request, res) => {
-		const transactions = await this.model.getTransationByCode(req.params.code);
+		const transactions = await this.model.getTransationByCode(
+			req.params.code
+		);
 		res.json(transactions);
 	};
 
@@ -108,7 +143,10 @@ export class TransactionController {
 				.json({ error: JSON.parse(result.error.message) });
 		}
 
-		const transaction = await this.model.create({...result.data, code_transaction: randomUUID()});
+		const transaction = await this.model.create({
+			...result.data,
+			code_transaction: randomUUID(),
+		});
 
 		if (transaction) {
 			return res
@@ -131,14 +169,14 @@ export class TransactionController {
 		const insertEgress = await this.model.create({
 			...resultValidation.data,
 			concept_id: 1,
-			code_transaction
+			code_transaction,
 		});
 
 		if (insertEgress) {
 			const insertIncome = await this.model.create({
 				...resultValidation.data,
 				concept_id: 2,
-				code_transaction
+				code_transaction,
 			});
 			if (insertIncome) {
 				return res
