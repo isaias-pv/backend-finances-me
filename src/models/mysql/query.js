66d0e141f4667cf73b4d35328709933ae5aa70e9
isaias-pv <alemais2003312@gmail.com>
2023-11-19 20:33:55 -0500

    Cambio de ubicación de base de datos

--- src/models/mysql/query.js
+++ src/models/mysql/query.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const DEFAULT_CONFIG = {
-	host: "localhost",
-	user: "root",
+	host: "sql10.freemysqlhosting.net",
+	user: "sql10663457",
 	port: 3306,
-	password: "2133002Isaias_",
-	database: "finance_db",
+	password: "4rB58uhXPC",
+	database: "sql10663457",
 };
 const connectionString = process.env.DATABASE_URL ?? DEFAULT_CONFIG;
 
